bbf1646ee220da7fd0f5d70a4b4f0f9ba6929d6e
Justin <apolongjustin49@gmail.com>
2025-12-05 14:47:39 +0800

    added a blinking line for a style

--- calculator/script.js
+++ calculator/script.js
@@ -2,4 +2,14 @@ const screen = document.getElementById('screen');
 
 function AddToScreen(num) {
   screen.innerText += num;
+};
+
+function Reset() {
+  screen.innerText = '';
+}
+
+function Delete() {
+  let text = screen.innerText;
+  let del = text.slice(0, text.length - 1);
+  screen.innerText = del;
 }
