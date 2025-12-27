11ff3506b508f860124756e9fdb1e46e88757584
Justin <apolongjsutin49@gmail.com>
2025-12-27 14:11:37 +0800

    deleted all relevant file and statrting from scratch

--- inventory-app/backend/middleware/middleware.js
+++
@@ -1,5 +0,0 @@
-import express from 'express';
-import cors from 'cors';
-
-export const parseJson = express.json();
-export const corsJson = cors({ origin: 'http://127.0.0.1:5500'});
--- inventory-app/backend/router/router.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { parseJson, corsJson } from '../middleware/middleware.js';
-import productRouter from '../routes/productRoutes.js';
-
-const router = Router();
-
-router.use('/products', parseJson, corsJson, productRouter);
-
-export default router;
