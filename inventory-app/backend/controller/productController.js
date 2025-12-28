40514cd8bb63c676e06334a1186f119fde7ce152
Justin <apolongjustin49@gmail.com>
2025-12-28 10:44:24 +0800

    finished the backend

--- inventory-app/backend/controller/productController.js
+++ inventory-app/backend/controller/productController.js
@@ -15,9 +15,12 @@ export const addProduct = async (req, res) => {
 
 export const getProducts = async (req, res) => {
   const result = await pool.query('SELECT * FROM products;');
+  const sorted = result.rows;
+
+  sorted.sort((a, b) => a.name.localeCompare(b.name));
 
   try {
-    res.status(202).json(result.rows);
+    res.status(202).json(sorted);
   } catch (error) {
     console.log(error);
     res.status(500).json({error: `Server can't be reached`});
@@ -40,3 +43,38 @@ export const deleteProduct = async (req, res) => {
     res.status(500).json({error: `Server can't be reached`});
   }
 };
+
+export const findProducts = async (req, res) => {
+  const productName = req.params.productName;
+  const regex = new RegExp(productName, 'gi');
+
+  try {
+    const result = await pool.query('SELECT * FROM products;');
+    const matched = result.rows.filter(items => regex.test(items.name));
+
+    if(matched.length === 0) return res.status(404).json({message: `No products named: ${productName}`});
+
+    res.json(matched);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({error: `Server can't be reached`});
+  }
+};
+
+export const updateProduct = async (req, res) => {
+  const productId = req.params.productId;
+  const { name, type, price, amount } = req.body;
+
+  try {
+    const result = await pool.query('UPDATE products SET name = $1, type = $2, price = $3, amount = $4 WHERE id = $5 RETURNING *;', [name, type, price, amount, productId]);
+
+    if(result.rows.length === 0) {
+      return res.status(404).json({message: 'Product not found'});
+    } 
+
+    res.status(200).json(result.rows[0]);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({error: `Server can't be reached`});
+  }
+}
--- inventory-app/backend/routes/productRoutes.js
+++ inventory-app/backend/routes/productRoutes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
-import { addProduct, deleteProduct, getProducts } from '../controller/productController.js';
+import { addProduct, deleteProduct, findProducts, getProducts, updateProduct } from '../controller/productController.js';
 
 const router = Router();
 
 router.post('/add', addProduct);
 router.get('/', getProducts);
 router.delete('/', deleteProduct);
+router.get('/:productName', findProducts);
+router.put('/:productId', updateProduct);
 
 export default router;
