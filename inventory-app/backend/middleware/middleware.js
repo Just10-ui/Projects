c3a0a94babd4a635f59582b6569d28b44531e9b6
Justin <apolongjustin49@gmail.com>
2025-12-29 11:58:20 +0800

    deleted all the files

--- inventory-app/backend/middleware/middleware.js
+++
@@ -1,5 +0,0 @@
-import express from 'express';
-import cors from 'cors';
-
-export const parseJson = express.json();
-export const corsJson = cors({origin: 'http://localhost:5500'});
--- inventory-app/backend/routes/routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { viewProduct } from '../controller/productRoutes.js';
-import { parseJson } from '../middleware/middleware.js';
-
-const router = Router();
-
-router.get('/', parseJson, viewProduct);
-
-export default router;
