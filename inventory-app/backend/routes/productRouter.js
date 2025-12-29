e75e8fe6e436fd3a04ad5e3ae16bf366f313a58f
Justin <apolongjustin49@gmail.com>
2025-12-29 22:52:47 +0800

    deleted a project

--- inventory-app/backend/routes/productRouter.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { findProducts, viewProducts } from '../controller/productController.js';
-
-const productRouter = Router();
-
-productRouter.get('/', viewProducts);
-productRouter.get('/:productName', findProducts);
-
-export default productRouter;
--- inventory-app/backend/routes/routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { parseJson } from '../middleware/middleware.js';
-import productRoutes from './productRouter.js';
-
-const router = Router();
-
-router.use('/products', parseJson, productRoutes);
-
-export default router;
