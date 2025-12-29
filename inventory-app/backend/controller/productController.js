1220d1c2fd684f67b2c7f30bba589f350fab743b
Justin <apolongjustin49@gmail.com>
2025-12-29 16:16:35 +0800

    added a findProduct function

--- inventory-app/backend/controller/productController.js
+++ inventory-app/backend/controller/productController.js
@@ -11,3 +11,25 @@ export const viewProducts = async (req, res) => {
     res.status(500).json({message: 'Database is not responding'});
   }
 };
+
+export const findProducts = async (req, res) => {
+  const productName = req.params.productName;
+
+  try {
+    const product = new RegExp(productName, 'gi');
+    const allProducts = await pool.query('SELECT * FROM products;');
+    const result = allProducts.rows.filter(items => {
+      const names = items.name.toLowerCase();
+      if (names.match(product)) return items;
+    });
+
+    if (result.length === 0) {
+      res.status(404).json({message: 'Products not found'});
+    } else {
+      res.status(200).json({message: 'Success', products: result});
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({error: 'Database is not responding'});
+  }
+};
--- inventory-app/backend/routes/productRouter.js
+++ inventory-app/backend/routes/productRouter.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import { viewProducts } from '../controller/productController.js';
+import { findProducts, viewProducts } from '../controller/productController.js';
 
 const productRouter = Router();
 
 productRouter.get('/', viewProducts);
+productRouter.get('/:productName', findProducts);
 
 export default productRouter;
