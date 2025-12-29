51375b53edb5a3512c90567568f06fc96b1cf0fa
Justin <apolongjustin49@gmail.com>
2025-12-29 12:53:02 +0800

    added a controller file

---
+++ inventory-app/backend/controller/productController.js
@@ -0,0 +1,13 @@
+import pool from '../database/db.js';
+
+export const viewProduct = async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM products;');
+    const sorted = result.rows.sort((a, b) => a.name.localeCompare(b.name));
+
+    res.status(200).json({message: 'Success', products: sorted});
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({message: 'Database is not responding'});
+  }
+};
